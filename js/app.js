591ca3c110df632f0eea818620a3a8735dcbbb0d
fracs31 <pist.fracs95@gmail.com>
2023-02-27 14:25:23 +0100

    ADD generation of the emails and print of the list of emails

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
 const { createApp } = Vue; //Vue
 //Vue app
 createApp({
+    //Dati
     data() {
         return {
-        
+            emails: [], //lista di email
         }
+    },
+    //Metodi
+    methods: {
+        //Metodo per ottenere le email dall'API
+        fetchEmail() {
+            //Ciclo
+            for (let i = 0; i < 10; i++) {
+                //Richiedo la email all'API
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((response) => {
+                    let email = response.data.response; //salvo l'email generata dall'API
+                    this.emails.push(email); //inserisco l'email generata dall'API dentro l'array di email
+                });
+            }
+        }
+    },
+    //Mounted
+    mounted() {
+        this.fetchEmail(); //prendo le email
     }
 }).mount('#app');
